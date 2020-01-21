ee708525238535f497fd6c5500a457d018c6cc5d
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 19:08:35 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,21 @@
-print("World")
-print("Hello")
-print("World")
+def find_two_smallest(L: [float]) -> Tuple[int, int]:
+    """ Return a tuple of the indices of the two smallest values in list L.
+    
+    items = [809, 834, 477, 478, 307, 122, 96, 102, 324, 476,]
+    
+    True
+    """
+    
+    #Get a sorted copy of the list so that the two smallest items are at the 
+    #front
+    temp_list = sorted(L)
+    smallest = temp_list[0]
+    next_smallest = temp_list[1]
+    
+    #Find the indices in the original list L
+    min1 = L.index(smallest)
+    min2 = L index(next_smallest)
+    
+    return(min1, min2)
+  
+  
